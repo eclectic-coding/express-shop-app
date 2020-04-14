b39550b3dc5e6e54e763c2b26e2ef5674bbece19
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-04-14 10:09:04 -0400

    Migrate to MongoDB.

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
-const path = require('path');
+const path = require('path')
 
-const express = require('express');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const errorController = require('./controllers/error');
+const errorController = require('./controllers/error')
+const mongoConnect = require('./util/database').mongoConnect
 
-const app = express();
+const app = express()
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req, res, next) => {
+  // User.findById(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
+  next()
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.get404);
+app.use(errorController.get404)
 
-app.listen(3000);
+mongoConnect(() => {
+  app.listen(4000)
+
+})
+
+
